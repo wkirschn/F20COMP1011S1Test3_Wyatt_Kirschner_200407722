8fafe2a48b7cfd7cb2f02e6ab0c6179dfdbc5cb4
Wyatt <wkirschn@uoguelph.ca>
2020-12-16 12:15:06 -0500

    Challenge 6 - Title and Icon are updated

--- F20COMP1011S1Test3StudentVersion/src/Main.java
+++ F20COMP1011S1Test3StudentVersion/src/Main.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import Utilities.AthleteUtility;
 
@@ -30,6 +31,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Views/athletesView.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.getIcons().add(new Image("Views/icon.png"));
+        stage.setTitle("Athlete Tracker Database - COMP1011");
         stage.show();
     }
 }
